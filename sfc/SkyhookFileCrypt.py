83aece56b8d10d50e85e4c90640d4213a5c47116
deedeecx330 <68820832+deedeecx330@users.noreply.github.com>
2020-07-27 14:18:54 +0200

    Update SkyhookFileCrypt.py

--- sfc/SkyhookFileCrypt.py
+++ sfc/SkyhookFileCrypt.py
@@ -2,42 +2,64 @@ import os, random
 from Crypto.Cipher import AES
 
 def encryptFile(originalName, newName, key):
-    if not os.path.isfile(originalName):
-        raise(FileNotFoundError("Could not find {}!".format(originalName)))
-    if len(newName) == 0:
-        raise(Exception("Cannot output to a nameless file!"))
+    
+    def pad(content):
+        return(content + b"\0" * (AES.block_size - len(content) % AES.block_size))
+
     separator = b" :><:><:><: "
-    bytesKey = bytes(str(key), 'utf8')
-    if len(bytesKey) == 0:
-        raise(Exception("Empty passphrase (key)!"))
-    elif len(bytesKey) < 32:
-        bytesKey = bytesKey + b"\0" * (32 - len(bytesKey))
-    else:
-        bytesKey = bytesKey[:-(len(bytesKey) - 32)]
-    with open(originalName, "rb") as infile:
-        iv = os.urandom(16)
-        cipher = AES.new(bytesKey, AES.MODE_CBC, iv)
-        content = infile.read()
-        encryptedContents = cipher.encrypt(content + b"\0" * (AES.block_size - len(content) % AES.block_size))
-    with open(newName, "wb") as outfile:
-        outfile.write(iv + separator + encryptedContents)
+
+    iv = os.urandom(16)
+
+    outFile = open(newName, "wb")
+    outFile.write(iv + separator)
+
+    inFile = open(originalName, "rb")
+
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    length = int(len(pad(inFile.read())) / AES.block_size)
+
+    for i in range(0, length):
+        inFile.seek(i * AES.block_size)
+        if i == length - 1:
+            toCrypt = pad(inFile.read(AES.block_size))
+        else:
+            toCrypt = inFile.read(AES.block_size)
+        encContent = cipher.encrypt(toCrypt)
+        outFile.write(encContent)
+    
+    inFile.close()
+    outFile.close()
 
 def decryptFile(oldName, newName, key):
-    if not os.path.isfile(oldName):
-        raise(FileNotFoundError("Could not find {}!".format(oldName)))
-    if len(newName) == 0:
-        raise(Exception("Cannot output to a nameless file!"))
+
+    def extractData(filename):
+        inFile = open(filename, "rb")
+        iv, content = inFile.read().split(separator)
+        inFile.close()
+        inFile = open(filename, "wb")
+        inFile.seek(0)
+        inFile.truncate()
+        inFile.write(content)
+        inFile.close()
+        inFile = open(filename, "rb")
+        length = int(len(content) / AES.block_size)
+        return(iv, length, inFile)
+
     separator = b" :><:><:><: "
-    bytesKey = bytes(str(key), 'utf8')
-    if len(bytesKey) == 0:
-        raise(Exception("Empty passphrase (key)!"))
-    elif len(bytesKey) < 32:
-        bytesKey = bytesKey + b"\0" * (32 - len(bytesKey))
-    else:
-        bytesKey = bytesKey[:-(len(bytesKey) - 32)]
-    with open(oldName, "rb") as infile:
-        iv, contents = infile.read().split(separator)
-        cipher = AES.new(bytesKey, AES.MODE_CBC, iv)
-        decryptedContents = cipher.decrypt(contents).rstrip(b"\0")
-    with open(newName, "wb") as outfile:
-        outfile.write(decryptedContents)
+
+    iv, length, inFile = extractData(oldName)
+    outFile = open(newName, "wb")
+
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+
+    for i in range(0, length):
+        inFile.seek(i * AES.block_size)
+        if i == (length - 1):
+            content = inFile.read(AES.block_size).rstrip(b"\0")
+        else:
+            content = inFile.read(AES.block_size)
+        decContent = cipher.decrypt(content)
+        outFile.write(decContent)
+
+    inFile.close()
+    outFile.close()
