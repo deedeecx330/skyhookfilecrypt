bf0cf5c251e1e0fe06542641060167a7e7b076ab
deedeecx330 <68820832+deedeecx330@users.noreply.github.com>
2020-07-28 17:10:05 +0200

    Switch to seek for better mem consumption

--- sfc/SkyhookFileCrypt.py
+++ sfc/SkyhookFileCrypt.py
@@ -9,14 +9,20 @@ def pad(content):
 def encryptFile(originalName, newName, key):
 
     iv = os.urandom(16)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
 
     outFile = open(newName, "wb")
     outFile.write(iv + separator)
 
     inFile = open(originalName, "rb")
+    inFile.seek(0, os.SEEK_END)
 
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    length = int(len(pad(inFile.read())) / AES.block_size)
+    contentLength = inFile.tell()
+
+    while contentLength % AES.block_size != 0:
+        contentLength = contentLength + 1
+
+    length = int(contentLength / AES.block_size)
 
     for i in range(0, length):
 
@@ -37,12 +43,15 @@ def encryptFile(originalName, newName, key):
 def decryptFile(oldName, newName, key):
 
     inFile = open(oldName, "rb")
-    iv, content = inFile.read().split(separator)
-
-    length = int(len(content) / AES.block_size)
+    iv = inFile.read(16)
 
     skip = len(iv + separator)
     cipher = AES.new(key, AES.MODE_CBC, iv)
+
+    inFile.seek(0, os.SEEK_END)
+
+    contentLength = inFile.tell() - skip
+    length = int(contentLength / AES.block_size)
 
     outFile = open(newName, "wb")
 
