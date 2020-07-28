bcd71bcc1d59fe78c4bb2edd9f18fede5de50573
deedeecx330 <68820832+deedeecx330@users.noreply.github.com>
2020-07-28 14:26:10 +0200

    ROll back on multithreading due to undefined bugs

--- sfc/SkyhookFileCrypt.py
+++ sfc/SkyhookFileCrypt.py
@@ -1,25 +1,7 @@
-import os, threading
+import os
 from Crypto.Cipher import AES
 
 separator = b" :><:><:><: "
-
-lock = threading.Lock()
-
-def enCrypt(aesCipher, outHandle, content, tthread):
-
-    encContent = aesCipher.encrypt(content)
-
-    if tthread == None:
-        with lock:
-            outHandle.write(encContent)
-            return(0)
-
-    while tthread.is_alive == True:
-        pass
-
-    with lock:
-        outHandle.write(encContent)
-        return(0)
 
 def pad(content):
     return(content + b"\x00" * (AES.block_size - len(content) % AES.block_size))
@@ -36,9 +18,6 @@ def encryptFile(originalName, newName, key):
     cipher = AES.new(key, AES.MODE_CBC, iv)
     length = int(len(pad(inFile.read())) / AES.block_size)
 
-    previous = None
-    threads = list()
-
     for i in range(0, length):
 
         inFile.seek(i * AES.block_size)
@@ -48,36 +27,12 @@ def encryptFile(originalName, newName, key):
         else:
             toCrypt = inFile.read(AES.block_size)
 
-        thread = threading.Thread(target=enCrypt, args=(cipher, outFile, toCrypt, previous,))
-        threads.append(thread)
-        previous = thread
-
-    for thread in threads:
-        thread.start()
-        thread.join()
+        toWrite = cipher.encrypt(toCrypt)
+        outFile.write(toWrite)
 
     inFile.close()
     outFile.close()
 
-def deCrypt(aesCipher, outHandle, content, tthread, padded):
-
-    decContent = aesCipher.decrypt(content)
-
-    if tthread == None:
-        with lock:
-            outHandle.write(decContent)
-            return(0)
-
-    while tthread.is_alive == True:
-        pass
-
-    with lock:
-
-        if padded == True:
-            decContent = decContent.rstrip(b"\x00")
-
-        outHandle.write(decContent)
-        return(0)
 
 def decryptFile(oldName, newName, key):
 
@@ -87,30 +42,21 @@ def decryptFile(oldName, newName, key):
     length = int(len(content) / AES.block_size)
 
     skip = len(iv + separator)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
 
     outFile = open(newName, "wb")
-
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    
-    previous = None
-    threads = list()
-    unPad = False
 
     for i in range(0, length):
 
         inFile.seek(i * AES.block_size + skip)
         content = inFile.read(AES.block_size)
 
+        toWrite = cipher.decrypt(content)
+
         if i == length - 1:
-            unPad = True
+            toWrite = toWrite.rstrip(b"\x00")
 
-        thread = threading.Thread(target=deCrypt, args=(cipher, outFile, content, previous, unPad,))
-        threads.append(thread)
-        previous = thread
-
-    for thread in threads:
-        thread.start()
-        thread.join()
+        outFile.write(toWrite)
 
     inFile.close()
     outFile.close()
