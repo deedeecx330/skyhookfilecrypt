5ca8f91ca8b904daf5d0898d63fd58461ca26b23
deedeecx330 <68820832+deedeecx330@users.noreply.github.com>
2020-07-30 16:57:39 +0200

    Publish to PyPi

--- README.md
+++ README.md
@@ -1,18 +1,14 @@
 # skyhookfilecrypt
-File encryption and decryption module extracted from Skyhook: https://github.com/deedeecx330/skyhook .
+File encryption and decryption module extracted from Skyhook: https://github.com/deedeecx330/skyhook
 
 # Usage
-To use skyhookfilecrypt, simply import it into your Python 3 project.
-
 The module has two functions, encryptFile and decryptFile. The function arguments in both cases are: 
 -   File that is to be encrypted/decrypted
 -   Output file where encrypted/decrypted contents will be saved to
 -   A passphrase to encrypt/decrypt the file with
 
-For example:
+For example
 ```
-import skyhookfilecrypt
-
 skyhookfilecrypt.encryptFile(file.in, file.out, password)
 ```
 will encrypt file.in with the given password and write the encrypted contents of file.in to file.out. Same principle works for decrypting files.
---
+++ setup.py
@@ -0,0 +1,25 @@
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    README = f.read()
+from setuptools import setup, find_packages
+
+setup(name='skyhookfilecrypt',
+    version='1.0',
+    packages = find_packages(),
+    description='File encryption and decryption module extracted from Skyhook',
+    long_description=README,
+    long_description_content_type='text/markdown',
+    author='deedeecx330',
+    url='https://github.com/deedeecx330/skyhookfilecrypt',
+    license='GNU General Public Licence v3 (GPLv3)',
+    install_requires=['pycrypto'],
+    keywords = "aes cbc encrypt decrypt cryptography file",
+    classifiers=[
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+        'Topic :: Security',
+        'Topic :: Security :: Cryptography',
+        'Topic :: Utilities',
+    ],
+)
